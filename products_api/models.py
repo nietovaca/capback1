2d3144541db9372316fdf141fbe825a330d60ebe
nietovaca <nietovaca@gmail.com>
2022-03-29 20:42:10 -0600

    changed model

--- products_api/models.py
+++ products_api/models.py
@@ -16,12 +16,13 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    amount = models.IntegerField(default=1)
 
 
     class Meta: 
