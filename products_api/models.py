f2985346cc68d4847b0626a7b5b81d707b1724d4
nietovaca <nietovaca@gmail.com>
2022-03-30 12:09:09 -0600

    changed category to not required

--- products_api/models.py
+++ products_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
-    category = models.CharField(max_length=255)
+    category = models.CharField(required=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     image = models.TextField(blank=True)
