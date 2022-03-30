fe53674fc49673ff8ea59204ae108cc25f26539c
nietovaca <nietovaca@gmail.com>
2022-03-30 11:58:27 -0600

    changed model

--- products_api/serializers.py
+++ products_api/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers 
 from .models import Product
-from .models import Category
-
-class CategorySerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
-    class Meta:
-        model = Category # tell django which model to use
-        fields = ('name',) # tell django which fields to include
-
 
 class ProductSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Product # tell django which model to use
-        fields = ('id', 'category', 'created_by', 'title', 'author', 'description', 'image', 'price', 'in_stock', 'is_active', 'created', 'updated', 'amount',) # tell django which fields to include
+        fields = ('id', 'category', 'title', 'description', 'image', 'price', 'in_stock', 'is_active', 'created', 'updated', 'amount',) # tell django which fields to include
