ac8c92ac4810984bc6ee7c867670ec4470aafdca
nietovaca <nietovaca@gmail.com>
2022-04-02 09:34:55 -0600

    default image

--- products_api/models.py
+++ products_api/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     category = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    primary_image = models.ImageField(blank=True, upload_to= 'img/')
+    primary_image = models.ImageField(upload_to= 'img/', default = '/img/motogirl.png')
     secondary_image = models.ImageField(blank=True, upload_to='img/')
     price = models.DecimalField(max_digits=6, decimal_places=2)
     in_stock = models.BooleanField(default=True)
