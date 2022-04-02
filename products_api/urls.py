50e5cea05fc705f9c8caa4cb8543fd5abfd8086b
nietovaca <nietovaca@gmail.com>
2022-04-02 09:28:24 -0600

    changed urls to path images

--- products_api/urls.py
+++ products_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import url, static
+
 from . import views
 
 urlpatterns = [
     path('api/products', views.ProductList.as_view(), name='product_list'),
     path('api/products/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
