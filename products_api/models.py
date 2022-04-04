617d6e6de38c5f2471576e5236d8393a63d14bab
nietovaca <nietovaca@gmail.com>
2022-04-04 10:05:25 -0600

    added img url field

--- products_api/models.py
+++ products_api/models.py
@@ -1,14 +1,16 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
     id = models.BigAutoField(primary_key=True)
-    category = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, default="New")
     title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    primary_image = models.ImageField(blank=True, upload_to= 'img/')
-    secondary_image = models.ImageField(blank=True, upload_to='img/')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True, default='')
+    primary_image = models.ImageField(blank=True, upload_to= 'img/',null = True)
+    secondary_image = models.ImageField(blank=True, upload_to='img/', null = True)
+    imgURL = models.TextField (default='', blank = True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=1.99)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
